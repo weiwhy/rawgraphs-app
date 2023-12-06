9c03d04cfc362da5dc8072eae9f35ecd3bd5a790
weiwhy <why1573920133@hotmail.com>
2023-12-06 11:08:21 +0800

    Update worker.js

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -1,5 +1,5 @@
 import * as Comlink from 'comlink'
-import { parseDataset, chart as rawChart } from '@rawgraphs/rawgraphs-core'
+import { parseDataset, chart as rawChart } from '@weiwhy/rawgraphs-core'
 import charts from '../charts'
 import { requireRawChartsFromUrlWebWorker } from '../hooks/rawRequire'
 
